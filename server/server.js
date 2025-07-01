1fd9846719b1a0a9a1ee55ba0bb436cc7e2621a3
KBhumee17 <bhumee.git@gmail.com>
2025-07-01 19:09:14 +0530

    server.js updated for vercel

--- server/server.js
+++ server/server.js
@@ -47,8 +47,12 @@ app.use("/api/auth", userRouter);
 app.use("/api/messages", messageRouter)
 
 //Connect to MongoDB
-await connectDB()
+await connectDB();
 
-const PORT = process.env.PORT || 5000;
+if(process.env.NODE_ENV !== "production"){
+    const PORT = process.env.PORT || 5000;
+    server.listen(PORT, ()=> console.log("Server is runnning on PORT:" + PORT));
+}
 
-server.listen(PORT, ()=> console.log("Server is runnning on PORT:" + PORT));
+//Export server for Vercel
+export default server;
