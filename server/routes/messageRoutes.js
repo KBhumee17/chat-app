034f5a49dd4c0eaf2241054b9f944deeca64c70d
KBhumee17 <bhumee.git@gmail.com>
2025-07-02 00:16:16 +0530

    messageRouter fix

--- server/routes/messageRoutes.js
+++ server/routes/messageRoutes.js
@@ -4,6 +4,11 @@ import { getMessages, getUsersForSidebar, markMessageAsSeen, sendMessage } from 
 
 const messageRouter = express.Router();
 
+// TEMP test route
+messageRouter.get("/", (req, res) => {
+  res.send("✅ Message route is working");
+});
+
 messageRouter.get("/users", protectRoute, getUsersForSidebar);
 messageRouter.get("/:id", protectRoute, getMessages);
 messageRouter.put("/mark/:id", protectRoute, markMessageAsSeen);
