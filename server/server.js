fd8590d993ea7155fd2b4c87bfdd592fad0aaea2
KBhumee17 <bhumee.git@gmail.com>
2025-07-02 00:23:42 +0530

    Final server.js with /api/messages route

--- server/server.js
+++ server/server.js
@@ -44,6 +44,7 @@ app.use("/api/status", (req, res) => res.send("Server is live"));
 app.use("/api/auth", userRouter);
 app.use("/api/messages", messageRouter);
 
+
 // Connect to DB
 await connectDB();
 
